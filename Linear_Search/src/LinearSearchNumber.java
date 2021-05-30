0ad6cf6327d906692d74197fa427549cef9e977d
falya amrina zahra <falya amrina zahra@LAPTOP-EMRHL9B8>
2021-05-30 09:58:31 +0700

    add to repo

---
+++ Linear_Search/src/LinearSearchNumber.java
@@ -0,0 +1,24 @@
+// CariLinear(L,N,X):
+// L adalah array dengan N elemen
+// X adalah data yang dicari
+// Nilai balik menyertakan posisi X dalam array L
+// atau -1 kalau tidak ditemukan 
+/**
+ *
+ * @author Ian Mulyana
+ */
+public class LinearSearchNumber{    
+    public static int linearSearch(int[] arr, int X){    
+            for(int i=0;i<arr.length;i++){    
+                if(arr[i] == X){    
+                    return i;    
+                }    
+            }    
+            return -1;    
+        }    
+        public static void main(String a[]){    
+            int[] L= {10,20,30,50,70,90};    
+            int X = 90;    
+            System.out.println(X+" Ditemukan Pada index: "+linearSearch(L, X));    
+        }    
+    }
